4200806e2d42c2a38cbe53eea35e617bd58d477d
Coby Randquist <randquistcp@gmail.com>
2012-09-30 18:00:21 -0700

    round two of changes for new auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,13 @@ class User < ActiveRecord::Base
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
   # attr_accessible :title, :body
 
   belongs_to :presenter
+
+  has_many :authentications
 end
