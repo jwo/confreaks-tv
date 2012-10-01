4b062b7502a5a51181f33284929d45b6b2f82cca
Coby Randquist <randquistcp@gmail.com>
2012-09-30 20:57:25 -0700

    fixes to make things work after devise cleanup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,22 @@ class ApplicationController < ActionController::Base
 
     @recent_videos = @recent_events.map {|event| event.videos.first}
   end
+
+  def login_required
+    if !current_user
+      respond_to do |format|
+        format.html {
+          redirect_to '/auth/confreaks_sso'
+        }
+        format.json {
+          render :json => { 'error' => 'Access Denied' }.to_json
+        }
+      end
+    end
+  end
+
+  def current_user
+    return nil unless session[:user_id]
+    @current_user ||= User.find_by_uid(session[:user_id]['uid'])
+  end
 end
