d1db1f9c209af18ffd997ca6061e1b15dd064d72
Coby Randquist <randquistcp@gmail.com>
2012-10-08 15:19:44 -1000

    turn off authentication validations

--- app/models/authentication.rb
+++ app/models/authentication.rb
@@ -3,9 +3,9 @@ class Authentication < ActiveRecord::Base
 
   belongs_to :user
 
-  validates :provider, :presence => true
-  validates :uid, :presence => true, :uniqueness => {:scope => :provider}
-  validates :user_id, :presence => true
+  #validates :provider, :presence => true
+  #validates :uid, :presence => true, :uniqueness => {:scope => :provider}
+  #validates :user_id, :presence => true
   
   def provider_name
     if provider == "openid"
